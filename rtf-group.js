23911941b24a5c52b9f037277db83b921648392b
Edmund LoPresti <edlopresti@ubicueinc.com>
2019-05-09 14:19:09 -0400

    Preference to prexisting styling
    Give preference to parent styles already defined or to preexisting styling on an object being added as content to the group.

--- rtf-group.js
+++ rtf-group.js
@@ -5,15 +5,25 @@ class RTFGroup {
     this.parent = parent
     this.content = []
     this.fonts = []
-    this.colors = []
-    this.style = {}
+    if (typeof parent !== 'undefined' && typeof parent.colors !== 'undefined') {
+        this.colors = parent.colors;
+    }
+    else {
+        this.colors = [];
+    }
+    if (typeof parent != 'undefined' && typeof parent.style !== 'undefined') {
+        this.style = parent.style;
+    }
+    else {
+        this.style = {};
+    }
     this.ignorable = null
   }
   get (name) {
     return this[name] != null ? this[name] : this.parent.get(name)
   }
   getFont (num) {
-    return this.fonts[num] != null ? this.fonts[num] : this.parent.getFont(num)
+    return this.fonts[num] != null && this.fonts[num] !== undefined ? this.fonts[num] : this.parent.getFont(num)
   }
   getColor (num) {
     return this.colors[num] != null ? this.colors[num] : this.parent.getFont(num)
@@ -26,10 +36,25 @@ class RTFGroup {
     this.style = {}
   }
   addContent (node) {
-    node.style = Object.assign({}, this.getStyle())
-    node.style.font = this.getFont(node.style.font)
-    node.style.foreground = this.getColor(node.style.foreground)
-    node.style.background = this.getColor(node.style.background)
+    if (typeof node.style == 'undefined') {
+        node.style = Object.assign({}, this.getStyle());
+    }
+    if (typeof node.style.font != 'undefined') {
+        //If we're not already being passed a Font object, retrieve a Font by numerical value
+        if (typeof node.style.font.name == 'undefined') {
+            node.style.font = this.getFont(node.style.font);
+        };
+    }
+    if (typeof node.style.foreground != 'undefined' && node.style.foreground != null) {
+        if (typeof node.style.foreground.red === 'undefined') {
+            node.style.foreground = this.getColor(node.style.foreground);
+        }
+    }
+    if (typeof node.style.background != 'undefined' && node.style.background != null) {
+        if (typeof node.style.background.red === 'undefined') {
+            node.style.background = this.getColor(node.style.background);
+        }
+    }
     this.content.push(node)
   }
 }
