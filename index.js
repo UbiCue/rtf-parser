ee57fe3e30ee162ced3b5d17ad660f03d3f4f403
Edmund LoPresti <edlopresti@ubicueinc.com>
2019-05-17 16:06:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,25 @@ parse.string = parseString
 parse.stream = parseStream
 
 function parseString (string, cb) {
-  parse(cb).end(string)
+  //parse(cb).end(string)
+  var parser = parse(cb);
+  var rtfParsed = parser.convert(string);
+  
+  const document = new RTFDocument()
+  const interpreter = new RTFInterpreter(document)
+
+  const errorHandler = err => {
+      if (errored) return
+      errored = true
+      cb(err)
+  };
+  interpreter.setErrorHandler(errorHandler);
+
+  for (var i = 0; i < rtfParsed.length; i++) {
+      interpreter.write(rtfParsed[i], null);
+  }
+
+  cb(null, document);
 }
 
 function parseStream (stream, cb) {
@@ -20,19 +38,19 @@ function parse (cb) {
   const errorHandler = err => {
     if (errored) return
     errored = true
-    parser.unpipe(interpreter)
-    interpreter.end()
+    //parser.unpipe(interpreter)
+    //interpreter.end()
     cb(err)
   }
-  const document = new RTFDocument()
+  //const document = new RTFDocument()
   const parser = new RTFParser()
   parser.once('error', errorHandler)
-  const interpreter = new RTFInterpreter(document)
-  interpreter.on('error', errorHandler)
-  interpreter.on('finish', () => {
-    if (!errored) cb(null, document)
-  })
-  parser.pipe(interpreter)
+  //const interpreter = new RTFInterpreter(document)
+  //interpreter.on('error', errorHandler)
+  //interpreter.on('finish', () => {
+  //  if (!errored) cb(null, document)
+  //})
+  //parser.pipe(interpreter)
   return parser
 }
 
