c8a9a7b216809d50218ada945ad6cb4ab75515a1
Edmund LoPresti <edlopresti@ubicueinc.com>
2019-05-22 09:14:42 -0400

    Update rtf-interpreter.js

--- rtf-interpreter.js
+++ rtf-interpreter.js
@@ -76,7 +76,7 @@ function RTFInterpreter(document) {
         }
     }
  
-  var finisher = function() {
+  this.finisher = function() {
     while (this.groupStack.length) this.cmd$groupEnd()
     const initialStyle = this.doc.content.length ? this.doc.content[0].style : []
     for (let prop of Object.keys(this.doc.style)) {
@@ -90,7 +90,7 @@ function RTFInterpreter(document) {
       if (match) this.doc.style[prop] = initialStyle[prop]
     }
   }
-  var flushHexStore = function() {
+  this.flushHexStore = function() {
     if (this.hexStore.length > 0) {
       let hexstr = this.hexStore.map(cmd => cmd.value).join('')
       this.group.addContent(new RTFSpan({
@@ -101,20 +101,20 @@ function RTFInterpreter(document) {
     }
   }
 
-  var cmd$groupStart = function() {
+  this.cmd$groupStart = function() {
     this.flushHexStore()
     if (this.group) this.groupStack.push(this.group)
     this.group = new RTFGroup(this.group || this.doc)
   }
-  var cmd$ignorable = function() {
+  this.cmd$ignorable = function() {
     this.flushHexStore()
     this.group.ignorable = true
   }
-  var cmd$endParagraph = function() {
+  this.cmd$endParagraph = function() {
     this.flushHexStore()
     this.group.addContent(new RTFParagraph())
   }
-  var cmd$groupEnd = function() {
+  this.cmd$groupEnd = function() {
     this.flushHexStore()
     const endingGroup = this.group
     this.group = this.groupStack.pop()
@@ -156,53 +156,53 @@ function RTFInterpreter(document) {
       }
     }
   }
-  var cmd$hexchar = function(cmd) {
+  this.cmd$hexchar = function(cmd) {
     this.hexStore.push(cmd)
   }
-  var cmd$error = function(cmd) {
+  this.cmd$error = function(cmd) {
     this.emit('error', new Error('Error: ' + cmd.value + (cmd.row && cmd.col ? ' at line ' + cmd.row + ':' + cmd.col : '') + '.'))
   }
 
-  var ctrl$rtf = function() {
+  this.ctrl$rtf = function() {
     this.group = this.doc
   }
 
   // new line
-  var ctrl$line = function() {
+  this.ctrl$line = function() {
     this.group.addContent(new RTFSpan({ value: '\n' }))
   }
 
   // alignment
-  var ctrl$qc = function() {
+  this.ctrl$qc = function() {
     this.group.style.align = 'center'
   }
-  var ctrl$qj = function() {
+  this.ctrl$qj = function() {
     this.group.style.align = 'justify'
   }
-  var ctrl$ql = function() {
+  this.ctrl$ql = function() {
     this.group.style.align = 'left'
   }
-  var ctrl$qr = function() {
+  this.ctrl$qr = function() {
     this.group.style.align = 'right'
   }
 
   // text direction
-  var ctrl$rtlch = function() {
+  this.ctrl$rtlch = function() {
     this.group.style.dir = 'rtl'
   }
-  var ctrl$ltrch = function() {
+  this.ctrl$ltrch = function() {
     this.group.style.dir = 'ltr'
   }
 
   // general style
-  var ctrl$par = function() {
+  this.ctrl$par = function() {
     //Create new paragraph, starting from document styling
     this.group.addContent(new RTFParagraph(this.doc))
   }
-  var ctrl$pard = function() {
+  this.ctrl$pard = function() {
     this.group.resetStyle()
   }
-  var ctrl$plain = function() {
+  this.ctrl$plain = function() {
     this.group.style.fontSize = this.doc.getStyle('fontSize')
     //When explicitly setting to plain, set all styles to false for group and span
     this.group.style.bold = false;
@@ -211,73 +211,73 @@ function RTFInterpreter(document) {
     this.spanStyle.bold = false;
     this.spanStyle.italic = false;
   }
-  var ctrl$b = function(set) {
+  this.ctrl$b = function(set) {
     this.group.style.bold = set !== 0
     this.spanStyle.bold = this.group.style.bold
   }
-  var ctrl$i = function(set) {
+  this.ctrl$i = function(set) {
     this.group.style.italic = set !== 0
     this.spanStyle.italic = this.group.style.italic
   }
-  var ctrl$u = function(num) {
+  this.ctrl$u = function(num) {
     var charBuf = Buffer.alloc ? Buffer.alloc(2) : new Buffer(2)
     // RTF, for reasons, represents unicode characters as signed integers
     // thus managing to match literally no one.
     charBuf.writeInt16LE(num, 0)
     this.group.addContent(new RTFSpan({value: iconv.decode(charBuf, 'ucs2')}))
   }
-  var ctrl$super = function() {
+  this.ctrl$super = function() {
     this.group.style.valign = 'super'
   }
-  var ctrl$sub = function() {
+  this.ctrl$sub = function() {
     this.group.style.valign = 'sub'
   }
-  var ctrl$nosupersub = function() {
+  this.ctrl$nosupersub = function() {
     this.group.style.valign = 'normal'
   }
-  var ctrl$strike = function(set) {
+  this.ctrl$strike = function(set) {
     this.group.style.strikethrough = set !== 0
   }
-  var ctrl$ul = function(set) {
+  this.ctrl$ul = function(set) {
     this.group.style.underline = set !== 0
   }
-  var ctrl$ulnone = function(set) {
+  this.ctrl$ulnone = function(set) {
     this.group.style.underline = false
   }
-  var ctrl$fi = function(value) {
+  this.ctrl$fi = function(value) {
     this.group.style.firstLineIndent = value
   }
-  var ctrl$cufi = function(value) {
+  this.ctrl$cufi = function(value) {
     this.group.style.firstLineIndent = value * 100
   }
-  var ctrl$li = function(value) {
+  this.ctrl$li = function(value) {
     this.group.style.indent = value
   }
-  var ctrl$lin = function(value) {
+  this.ctrl$lin = function(value) {
     this.group.style.indent = value
   }
-  var ctrl$culi = function(value) {
+  this.ctrl$culi = function(value) {
     this.group.style.indent = value * 100
   }
-  var ctrl$tab = function() {
+  this.ctrl$tab = function() {
       var spacer = { value: "&nbsp;", style: this.group.style };
       this.group.addContent(new RTFSpan(spacer));
   }
 
 // encodings
-  var ctrl$ansi = function() {
+  this.ctrl$ansi = function() {
     this.group.charset = 'ASCII'
   }
-  var ctrl$mac = function() {
+  this.ctrl$mac = function() {
     this.group.charset = 'MacRoman'
   }
-  var ctrl$pc = function() {
+  this.ctrl$pc = function() {
     this.group.charset = 'CP437'
   }
-  var ctrl$pca = function() {
+  this.ctrl$pca = function() {
     this.group.charset = 'CP850'
   }
-  var ctrl$ansicpg = function(codepage) {
+  this.ctrl$ansicpg = function(codepage) {
     if (availableCP.indexOf(codepage) === -1) {
       this.emit('error', new Error('Codepage ' + codepage + ' is not available.'))
     } else {
@@ -286,10 +286,10 @@ function RTFInterpreter(document) {
   }
 
 // fonts
-  var ctrl$fonttbl = function() {
+  this.ctrl$fonttbl = function() {
     this.group = new FontTable(this.group.parent)
   }
-  var ctrl$f = function(num) {
+  this.ctrl$f = function(num) {
     if (this.group instanceof FontTable) {
       this.group.currentFont = this.group.table[num] = new Font()
     } else if (this.group.parent instanceof FontTable) {
@@ -298,47 +298,47 @@ function RTFInterpreter(document) {
       this.group.style.font = num
     }
   }
-  var ctrl$fnil = function() {
+  this.ctrl$fnil = function() {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').family = 'nil'
     }
   }
-  var ctrl$froman = function() {
+  this.ctrl$froman = function() {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').family = 'roman'
     }
   }
-  var ctrl$fswiss = function() {
+  this.ctrl$fswiss = function() {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').family = 'swiss'
     }
   }
-  var ctrl$fmodern = function() {
+  this.ctrl$fmodern = function() {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').family = 'modern'
     }
   }
-  var ctrl$fscript = function() {
+  this.ctrl$fscript = function() {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').family = 'script'
     }
   }
-  var ctrl$fdecor = function() {
+  this.ctrl$fdecor = function() {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').family = 'decor'
     }
   }
-  var ctrl$ftech = function() {
+  this.ctrl$ftech = function() {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').family = 'tech'
     }
   }
-  var ctrl$fbidi = function() {
+  this.ctrl$fbidi = function() {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').family = 'bidi'
     }
   }
-  var ctrl$fcharset = function(code) {
+  this.ctrl$fcharset = function(code) {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       let charset = null
       if (code === 1) {
@@ -352,63 +352,63 @@ function RTFInterpreter(document) {
       this.group.get('currentFont').charset = charset
     }
   }
-  var ctrl$fprq = function(pitch) {
+  this.ctrl$fprq = function(pitch) {
     if (this.group instanceof FontTable || this.group.parent instanceof FontTable) {
       this.group.get('currentFont').pitch = pitch
     }
   }
 
   // colors
-  var ctrl$colortbl = function() {
+  this.ctrl$colortbl = function() {
     this.group = new ColorTable(this.group.parent)
   }
-  var ctrl$red = function(value) {
+  this.ctrl$red = function(value) {
     if (this.group instanceof ColorTable) {
       this.group.red = value
     }
   }
-  var ctrl$blue = function(value) {
+  this.ctrl$blue = function(value) {
     if (this.group instanceof ColorTable) {
       this.group.blue = value
     }
   }
-  var ctrl$green = function(value) {
+  this.ctrl$green = function(value) {
     if (this.group instanceof ColorTable) {
       this.group.green = value
     }
   }
-  var ctrl$cf = function(value) {
+  this.ctrl$cf = function(value) {
     this.group.style.foreground = value
   }
-  var ctrl$cb = function(value) {
+  this.ctrl$cb = function(value) {
     this.group.style.background = value
   }
-  var ctrl$fs = function(value) {
+  this.ctrl$fs = function(value) {
     this.group.style.fontSize = value
   }
 
 // margins
-  var ctrl$margl = function(value) {
+  this.ctrl$margl = function(value) {
     this.doc.marginLeft = value
   }
-  var ctrl$margr = function(value) {
+  this.ctrl$margr = function(value) {
     this.doc.marginRight = value
   }
-  var ctrl$margt = function(value) {
+  this.ctrl$margt = function(value) {
     this.doc.marginTop = value
   }
-  var ctrl$margb = function(value) {
+  this.ctrl$margb = function(value) {
     this.doc.marginBottom = value
   }
 
 // unsupported (and we need to ignore content)
-  var ctrl$stylesheet = function(value) {
+  this.ctrl$stylesheet = function(value) {
     this.group.ignorable = true
   }
-  var ctrl$info = function(value) {
+  this.ctrl$info = function(value) {
     this.group.ignorable = true
   }
-  var ctrl$mmathPr = function(value) {
+  this.ctrl$mmathPr = function(value) {
     this.group.ignorable = true
   }
 }
