24e20ac14435e0adcaa3c82cfb3b0a7a89b3bf1f
Edmund LoPresti <edlopresti@ubicueinc.com>
2019-05-22 09:36:00 -0400

    Update rtf-interpreter.js

--- rtf-interpreter.js
+++ rtf-interpreter.js
@@ -125,6 +125,8 @@ function RTFInterpreter(document) {
       doc.colors = endingGroup.table
     } else if (endingGroup !== this.doc && !endingGroup.get('ignorable')) {
       for (const item of endingGroup.content) {
+        console.log(typeof doc);
+        console.log(typeof doc.docAddContent);
         doc.docAddContent(item)
       }
       process.emit('debug', 'GROUP END', endingGroup.type, endingGroup.get('ignorable'))
