ae460e5f49a8a89b376c5907add3d3de5f032ede
Edmund LoPresti <edlopresti@ubicueinc.com>
2019-05-24 12:10:31 -0400

    Update rtf-interpreter.js

--- rtf-interpreter.js
+++ rtf-interpreter.js
@@ -57,39 +57,56 @@ function RTFInterpreter(document) {
   this.once('prefinish', function () { this.finisher(); })
   this.hexStore = []
   this.spanStyle = {}
-  this._write = function(cmd, encoding, done) {
-    const method = 'cmd$' + cmd.type.replace(/-(.)/g, (_, char) => char.toUpperCase())
-    if (this[method]) {
-      this[method](cmd)
-    } else {
-      process.emit('error', `Unknown RTF command ${cmd.type}, tried ${method}`)
-    }
-    done()
-  }
-  this.write = function(cmd, encoding) {
-        const method = 'cmd$' + cmd.type.replace(/-(.)/g, (_, char) => char.toUpperCase());
-        if (this[method]) {
-            this[method](cmd)
-        } else {
-            //process.emit('error', `Unknown RTF command ${cmd.type}, tried ${method}`)
-            console.log(`Unknown RTF command ${cmd.type}, tried ${method}`);
-        }
-    }
+  this.deriveCmd = function(type) {
+	let command = '';
+		for (var i=0; i<type.length; i++) {
+			if ((type[i] == '-') && (i<type.length+1)) {
+				command += type[i+1].toUpperCase();
+				i++;
+			}
+			else {
+				command += type[i];
+			}
+		}
+		//const method = 'cmd$' + cmd.type.replace(/-(.)/g, (_, char) => char.toUpperCase());
+		return 'cmd$'+command;
+	}
+	this._write = function(cmd, encoding, done) {
+		let method = this.deriveCmd(cmd.type);
+		if (this[method]) {
+		  this[method](cmd)
+		} else {
+		  process.emit('error', "Unknown RTF command "+cmd.type+", tried "+method)
+		}
+		done()
+	}
+	this.write = function(cmd, encoding) {
+		//const method = 'cmd$' + cmd.type.replace(/-(.)/g, (_, char) => char.toUpperCase());
+		let method = this.deriveCmd(cmd.type);
+		if (this[method]) {
+			this[method](cmd)
+		} else {
+			//process.emit('error', `Unknown RTF command ${cmd.type}, tried ${method}`)
+			console.log("Unknown RTF command "+cmd.type+", tried "+method);
+		}
+	}
  
-  this.finisher = function() {
-    while (this.groupStack.length) this.cmd$groupEnd()
-    const initialStyle = this.doc.content.length ? this.doc.content[0].style : []
-    for (let prop of Object.keys(this.doc.style)) {
-      let match = true
-      for (let para of this.doc.content) {
-        if (initialStyle[prop] !== para.style[prop]) {
-          match = false
-          break
-        }
-      }
-      if (match) this.doc.style[prop] = initialStyle[prop]
-    }
-  }
+	this.finisher = function() {
+		while (this.groupStack.length) this.cmd$groupEnd()
+		const initialStyle = this.doc.content.length ? this.doc.content[0].style : []
+		for (let i=0; i<Object.keys(this.doc.style).length;i++) {
+			let prop = Object.keys(this.doc.style)[i];
+			let match = true
+			for (let j=0; j<this.doc.content.length; j++) {
+				let para = this.doc.content[j];
+				if (initialStyle[prop] !== para.style[prop]) {
+				  match = false
+				  break
+				}
+			}
+			if (match) this.doc.style[prop] = initialStyle[prop]
+		}
+	}
   this.flushHexStore = function() {
     if (this.hexStore.length > 0) {
       let hexstr = this.hexStore.map(cmd => cmd.value).join('')
@@ -461,6 +478,96 @@ function ColorTable(parent) {
     this.blue = 0
     this.green = 0
   }
+}
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.19
+// Reference: http://es5.github.io/#x15.4.4.19
+if (!Array.prototype.map) {
+
+  Array.prototype.map = function(callback/*, thisArg*/) {
+
+    var T, A, k;
+
+    if (this == null) {
+      throw new TypeError('this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling ToObject passing the |this| 
+    //    value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get internal 
+    //    method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (arguments.length > 1) {
+      T = arguments[1];
+    }
+
+    // 6. Let A be a new array created as if by the expression new Array(len) 
+    //    where Array is the standard built-in constructor with that name and 
+    //    len is the value of len.
+    A = new Array(len);
+
+    // 7. Let k be 0
+    k = 0;
+
+    // 8. Repeat, while k < len
+    while (k < len) {
+
+      var kValue, mappedValue;
+
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal 
+      //    method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal 
+        //    method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Let mappedValue be the result of calling the Call internal 
+        //     method of callback with T as the this value and argument 
+        //     list containing kValue, k, and O.
+        mappedValue = callback.call(T, kValue, k, O);
+
+        // iii. Call the DefineOwnProperty internal method of A with arguments
+        // Pk, Property Descriptor
+        // { Value: mappedValue,
+        //   Writable: true,
+        //   Enumerable: true,
+        //   Configurable: true },
+        // and false.
+
+        // In browsers that support Object.defineProperty, use the following:
+        // Object.defineProperty(A, k, {
+        //   value: mappedValue,
+        //   writable: true,
+        //   enumerable: true,
+        //   configurable: true
+        // });
+
+        // For best browser support, use the following:
+        A[k] = mappedValue;
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+
+    // 9. return A
+    return A;
+  };
 }
 
 module.exports = RTFInterpreter
