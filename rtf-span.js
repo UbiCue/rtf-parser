4227edb5cf90240386c16f79dad328904241529c
Edmund LoPresti <edlopresti@ubicueinc.com>
2019-06-12 15:46:58 -0400

    Update rtf-span.js

--- rtf-span.js
+++ rtf-span.js
@@ -3,7 +3,9 @@
 function RTFSpan(opts) {
   if (!opts) opts = {}
   this.value = opts.value
-  this.style = Object.assign({}, opts.style)
+  for (var attr in opts.style) {
+    this.style[attr] = opts.style[attr];
+  }
 }
 
 module.exports = RTFSpan
